bbad021657420bc4475eaae7a99fc1fd941d38c0
Reuben <4957964+reubn@users.noreply.github.com>
2020-06-09 22:00:51 +0100

    🙃 + Env

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 export const mDNSNetwork = {
-  IPv4: {interface: '0.0.0.0'},
-  IPv6: {ip: 'ff02::fb', interface: '::%eth0', type: 'udp6'}
+  IPv4: {interface: process.env.IPv4_INTERFACE || '0.0.0.0'},
+  IPv6: {ip: process.env.IPv6_IP || 'ff02::fb', interface: process.env.IPv6_INTERFACE || '::%eth0', type: 'udp6'}
 }
 
-export const DNSPort = 9999
-export const DNSAddress = '0.0.0.0'
+export const DNSPort = process.env.DNS_PORT || 9999
+export const DNSAddress = process.env.DNS_ADDRESS || '0.0.0.0'
 
 // Root on DNS Side
-export const DNSRootDomain = 'cedar'
+export const DNSRootDomain = process.env.DNS_ROOT_DOMAIN || 'cedar'
 
 // Root on mDNS Side
-export const mDNSRootDomain = 'local'
+export const mDNSRootDomain = process.env.mDNS_ROOT_DOMAIN || 'local'
 
 // TTL sent to clients
-export const DNSTTL = 300 // sec
+export const DNSTTL = +process.env.DNS_TTL || 300 // sec
 
 // How long are mDNS results kept for
-export const mDNSTTL = 60 * 60 * 1000 // ms
+export const mDNSTTL = +process.env.mDNS_TTL || 60 * 60 * 1000 // ms
 
 // How long may a mDNS lookup take before conceeding the address doesn't exist
-export const mDNSTimeout = 3 * 1000 // ms
+export const mDNSTimeout = +process.env.mDNS_TIMEOUT || 3 * 1000 // ms
